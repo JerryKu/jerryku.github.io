707d9db3198017648895c494b825ea2d48d840a1
JerryKu <jerryku1220@gmail.com>
2017-08-03 06:02:41 -0700

    Updated Resume and Bio

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-	$('#project').on('click', function(){
-		$('#page4').slideToggle(1000);
+	$('.arrow').on('click', function(){
+		$('#page3').slideToggle(1000);
+	});
+
+	$("#page3 .tech span").addClass("bordered");
+
+	$("#portfoliolink").on("click",function(){
+		$("#page3").slideToggle(1000);
+	});
+
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
 	});
 });
